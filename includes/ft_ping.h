4a41ab741048c313709659adab6b09f99dcb8bed
JeremShy <jcamhi@student.42.fr>
2017-12-21 13:30:11 -0500

    Rainbow Dash is best pony !

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -1,6 +1,23 @@
 #ifndef FILLER_H
 # define FILLER_H
 
-#include <libft.h>
+# include <libft.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <sys/time.h>
+# include <netdb.h>
+# include <unistd.h>
+# include <arpa/inet.h>
+# include <stdlib.h>
+# include <signal.h>
+
+# define NO_OPT	0
+# define OPT_v	1
+# define OPT_h	2
+
+typedef struct s_data {
+	int	opt;
+}								t_data;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,30 @@
 #include <ft_ping.h>
 
-int main() {
-	printf("%s\n", ft_strdup("coucoo"));
+void print_help(char *av)
+{
+	printf("usage: %s [-h] [-v]\n", av);
+}
+
+int init_data(t_data *data, char **av)
+{
+	data->opt = 0;
+	return (1);
+}
+
+int main(int ac, char **av) {
+	t_data data;
+
+	if (ac == 1)
+	{
+		print_help(av[0]);
+		return (1);
+	}
+	if (!init_data(&data, av))
+	{
+		return (1);
+	}
+	if (data.opt & OPT_v)
+	{
+		printf("v\n");
+	}
 }
