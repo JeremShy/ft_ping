c6e5e1f79f9615fc0233ce6f13741316e19b7391
JeremShy <jcamhi@student.42.fr>
2017-12-21 13:38:49 -0500

    Rainbow Dash is best pony !

--- srcs/main.c
+++ srcs/main.c
@@ -5,9 +5,29 @@ void print_help(char *av)
 	printf("usage: %s [-h] [-v]\n", av);
 }
 
-int init_data(t_data *data, char **av)
+int init_data(t_data *data, int ac, char **av)
 {
+	int	i;
+	int	j;
+
 	data->opt = 0;
+	i = 1;
+	while (i < ac && !ft_strequ(av[i], "--"))
+	{
+		if (av[i][0] == '-')
+		{
+			j = 1;
+			while (av[i][j])
+			{
+				if (av[i][j] == 'h')
+					data->opt |= OPT_h;
+				else if (av[i][j] == 'v')
+					data->opt |= OPT_v;
+				j++;
+			}
+		}
+		i++;
+	}
 	return (1);
 }
 
@@ -19,12 +39,16 @@ int main(int ac, char **av) {
 		print_help(av[0]);
 		return (1);
 	}
-	if (!init_data(&data, av))
+	if (!init_data(&data, ac, av))
 	{
 		return (1);
 	}
 	if (data.opt & OPT_v)
 	{
 		printf("v\n");
+	}
+	if (data.opt & OPT_h)
+	{
+		printf("h\n");
 	}
 }
