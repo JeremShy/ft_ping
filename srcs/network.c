0642aa8bdc1de5686b445e97e149266ffd9f6fbd
jcamhi <jeremshy@h3lp.fr>
2017-12-28 19:18:14 +0100

    tcpdump me l'affiche !

--- srcs/network.c
+++ srcs/network.c
@@ -3,12 +3,34 @@
 #include <netinet/ip_icmp.h>
 #include <netinet/ip.h>
 
+u_int16_t checksum(void *dgram, size_t size)
+{
+	u_int16_t *tmp;
+	size_t sum;
+
+	tmp = dgram;
+	sum = 0;
+	while (size > 1)
+	{
+		sum += *tmp;
+		tmp++;
+		size -= 2;
+	}
+	if ( size == 1 )
+		sum += *(unsigned char*)tmp;
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	return (~sum);
+}
+
 int	create_socket(t_data *data)
 {
 	int	sock;
 	struct addrinfo hints, *res;
-	char dgram[4096];
+	char dgram[64];
 
+	char *msg;
+	bzero(dgram, sizeof(dgram));
 	sock = socket(AF_INET, SOCK_RAW, 1); // proto ICMP : 1
 	printf ("sock returned: %d\n", sock);
 
@@ -24,15 +46,14 @@ int	create_socket(t_data *data)
 	printf("%s\n", inet_ntop(AF_INET, &in_addr, dst, 19));
 
 	struct icmphdr *icmp = (struct icmphdr *)dgram;
+	msg = dgram + sizeof(struct icmphdr);
 	icmp->type = ICMP_ECHO;
 	icmp->code = 0;
-	icmp->checksum = htons(~(ICMP_ECHO << 8));
-
-
-
-	if (connect(sock, saddr, sizeof(struct sockaddr)) != 0) {
-		printf ("connect failed !");
-	}
+	icmp->un.echo.id = htons(data->pid);
+	icmp->un.echo.sequence = 0;
+	strcpy(msg, "coucou");
+	icmp->checksum = htons(checksum(dgram, sizeof(dgram)));
+	sendto(sock, dgram, sizeof(dgram), 0, saddr, sizeof(struct sockaddr));
 
 
 	return (sock);
