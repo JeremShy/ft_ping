5003dd748167cad191c5b78ec810031fa208bfb5
JeremShy <jcamhi@student.42.fr>
2018-01-20 00:50:50 +0100

    Rainbow Dash is best pony !

--- srcs/network.c
+++ srcs/network.c
@@ -23,34 +23,25 @@ u_int16_t checksum(void *dgram, size_t size)
 	return (~sum);
 }
 
-int analyse_icmp_response(t_data *data, char buffer[200], struct timeval recvtime, int r, int expected_size)
+
+// if (icmp->code != 0 || icmp->type != 0 || r != 64 + sizeof(struct iphdr))
+// {
+// 	if (data->opt & OPT_v)
+// 		printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.1f ms : type %d, code : %d\n", 64, dst, ntohs(icmp->un.echo.sequence), ip->ttl, diff, icmp->type, icmp->code);
+// }
+
+
+void print_ok_msg(t_data *data, char buffer[200], struct timeval recvtime)
 {
+	float	diff;
 	struct icmphdr *icmp;
 	struct iphdr *ip;
 	struct timeval *tv;
-	float diff;
-	char *msg;
-	u_int16_t check;
 	char dst[40];
 
 	ip = (struct iphdr*)buffer;
 	icmp = (void*)buffer + ip->ihl * 4;
 	tv = (void*)buffer + ip->ihl * 4 + sizeof(struct icmphdr);
-	msg = (void*)buffer + ip->ihl * 4 + sizeof(struct icmphdr) + sizeof(struct timeval);
-
-	print_icmp_hdr(icmp);
-	if (ip->protocol != 1)
-		return (0);
-	if (r != expected_size && ntohs(icmp->un.echo.id) != data->pid && (icmp->type == 0))
-		return (-1);
-	if (r != expected_size && !ft_strequ(msg, "coucou") && (icmp->type == 0))
-		return (-1);
-
-	check = icmp->checksum;
-	icmp->checksum = 0;
-	if (r != expected_size && checksum(icmp, r - sizeof(struct iphdr)) != check)
-		return (-1);
-
 	if (ip->version == 4)
 	{
 		struct in_addr *addr;
@@ -65,26 +56,50 @@ int analyse_icmp_response(t_data *data, char buffer[200], struct timeval recvtim
 		addr = (void*)&(ip->saddr);
 		inet_ntop(AF_INET6, addr, dst, sizeof(dst));
 	}
-	return (1);
-
-	/*
-		TODO :	 1 = Ok, print message
-						 0 = Discard message
-						-1 = Show error message
-	*/
-
 	diff = ((recvtime.tv_sec * 1000  + recvtime.tv_usec / 1000.0) - (tv->tv_sec * 1000  + tv->tv_usec / 1000.0));
-	if (icmp->code != 0 || icmp->type != 0 || r != 64 + sizeof(struct iphdr))
-	{
-		if (data->opt & OPT_v)
-			printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.1f ms : type %d, code : %d\n", 64, dst, ntohs(icmp->un.echo.sequence), ip->ttl, diff, icmp->type, icmp->code);
-	}
-	else
-	{
-		printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.1f ms\n", 64, dst, ntohs(icmp->un.echo.sequence), ip->ttl, diff);
-		data->lst = add_pckt(data->lst, create_pckt(ntohs(icmp->un.echo.sequence), diff));
-		data->nreceived++;
-	}
+	printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.1f ms\n", 64, dst, ntohs(icmp->un.echo.sequence), ip->ttl, diff);
+	data->lst = add_pckt(data->lst, create_pckt(ntohs(icmp->un.echo.sequence), diff));
+	data->nreceived++;
+}
+
+int analyse_icmp_response(t_data *data, char buffer[200], int r, int expected_size)
+{
+	struct icmphdr *icmp;
+	struct iphdr *ip;
+	char *msg;
+	u_int16_t check;
+
+	ip = (struct iphdr*)buffer;
+	icmp = (void*)buffer + ip->ihl * 4;
+	msg = (void*)buffer + ip->ihl * 4 + sizeof(struct icmphdr) + sizeof(struct timeval);
+
+	print_icmp_hdr(icmp);
+	if (ip->protocol != 1)
+		return (0);
+	if (r != expected_size && ntohs(icmp->un.echo.id) != data->pid)
+		return (-1);
+	if (r != expected_size && !ft_strequ(msg, "coucou"))
+		return (-1);
+
+	check = icmp->checksum;
+	icmp->checksum = 0;
+	if (r != expected_size && checksum(icmp, r - sizeof(struct iphdr)) != check)
+		return (-1);
+	return (1);
+}
+
+void print_error_message(t_data *data, char buffer[200])
+{
+	struct icmphdr *icmp;
+	struct iphdr *ip1;
+	struct iphdr *ip2;
+	struct timeval *tv;
+
+	ip1 = (struct iphdr*)buffer;
+	icmp = (void*)buffer + ip1->ihl * 4;
+	ip2 = (void*)buffer + ip1->ihl * 4 + sizeof(struct icmphdr);
+	tv = (void*)buffer + ip1->ihl * 4 + sizeof(struct icmphdr) + ip2->ihl * 4;
+	print_memory((char*)tv, 8);
 }
 
 void recv_echo_response(t_data *data)
@@ -93,6 +108,7 @@ void recv_echo_response(t_data *data)
 	char buffer[200];
 	struct timeval recvtime;
 	struct iovec iov;
+	int		analysis;
 
 	ft_bzero(buffer, sizeof(buffer));
 	ft_bzero(&msghdr, sizeof(msghdr));
@@ -110,10 +126,23 @@ void recv_echo_response(t_data *data)
 
 	buffer[r] = 0;
 
-	if (r != 64 + sizeof(struct iphdr))
-		dprintf(2, "Error ! r = %d, struct iphdr = %lu, struct icmphdr = %lu\n", r, sizeof(struct iphdr), sizeof(struct icmphdr));
-	print_memory(buffer, r);
-	analyse_icmp_response(data, buffer, recvtime, r, 64 + sizeof(struct iphdr));
+	// if (r != 64 + sizeof(struct iphdr))
+	// 	dprintf(2, "Error ! r = %d, struct iphdr = %lu, struct icmphdr = %lu\n", r, sizeof(struct iphdr), sizeof(struct icmphdr));
+	// print_memory(buffer, r);
+	analysis = analyse_icmp_response(data, buffer, r, 64 + sizeof(struct iphdr));
+	printf("analysis = %d\n", analysis);
+	if (analysis == 1)
+	{
+		print_ok_msg(data, buffer, recvtime);
+	}
+	else if (analysis == -1)
+	{
+		print_error_message(data, buffer);
+	}
+	else
+	{
+		printf("this packet must be discarded.\n");
+	}
 }
 
 int send_echo_request(t_data *data)
@@ -139,6 +168,8 @@ int send_echo_request(t_data *data)
 		return (0);
 	}
 	icmp->checksum = checksum(dgram, sizeof(dgram));
+	printf("on send\n");
+	print_memory((char*)tv, 8);
 	sendto(data->sock, dgram, sizeof(dgram), 0, data->res->ai_addr, data->res->ai_addrlen);
 	data->ntransmitted++;
 	return (1);
